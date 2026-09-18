9e03d6bbcf173bb0077118013b932e9456186d3d
SnigdhaKubal05 <snigdhakubal31@gmail.com>
2026-09-19 00:14:42 +0530

    color change

--- heart.py
+++ heart.py
@@ -2,16 +2,30 @@ import turtle
 import math
 
 screen = turtle.Screen()
+screen.setup(width=850, height=700)
 screen.bgcolor("black")
 
 t = turtle.Turtle()
 t.speed(1)
 t.hideturtle()
 t.penup()
-t.color("#ffb6c1")
 
-for scale in range(11, 17):
+# Different shades of pink
+pink_colors = [
+    "#ffb6c1",
+    "#ff91a4",
+    "#ff69b4",
+    "#ff4f81",
+    "#ff1493",
+    "#ff007f"
+]
+
+for scale in range(15, 26):
     for i in range(120):
+
+        # Change color for each "I love you"
+        t.color(pink_colors[i % len(pink_colors)])
+
         angle = i * (math.pi * 2) / 120
 
         x = 16 * (math.sin(angle) ** 3) * scale
@@ -22,8 +36,11 @@ for scale in range(11, 17):
              - math.cos(4 * angle)) * scale
 
         t.goto(x, y)
-        t.write("I love you",
-                align="center",
-                font=("Arial", 8, "bold"))
+
+        t.write(
+            "I love you",
+            align="center",
+            font=("Arial", 12, "bold")
+        )
 
 turtle.done()
